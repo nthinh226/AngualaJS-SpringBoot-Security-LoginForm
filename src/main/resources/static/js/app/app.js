a13279ae5b87ab02b27d7ee71956b1ab8bcc187a
MSI-NT <ngocthinh1126@gmail.com>
2023-02-17 08:54:22 +0700

    delete comment

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -13,10 +13,6 @@ app.controller('ModalContentCtrl', function ($scope, $uibModalInstance) {
 });
 
 app.controller('productController', ['$scope', '$uibModal', '$http', function ($scope, $uibModal, $http) {
-//            $http.get('/get-products').
-//                    then(function (response) {
-//                        $scope.products = response.data;
-//                    });
 
         $http({
             method: "GET",
@@ -28,30 +24,10 @@ app.controller('productController', ['$scope', '$uibModal', '$http', function ($
         }).then(function (res) {
             console.log(res);
             $scope.products = res.data;
-//                let closeLoader = setTimeout(function () {
-//                    document.getElementById("loaderInnerList").style.display = "none";
-//                    clearTimeout(closeLoader);
-//                }, 100);
-
         }, function (error) {
             console.log(error);
         });
-//            $scope.selectRow = function (item) {
-//                console.log('Selected item: ' + item);
-//            };
-
-//        $scope.openModal = function (product) {
-//            $scope.selectedProduct = product;
-//            console.log(product);
-////
-//            $("#productModal").modal("show");
-////                    var modalInstance = $uibModal.open({
-////                        templateUrl: '/products.html',
-////                        scope: $scope
-////                    });
-//
-//        };
-
+        
         $scope.openModal = function (product) {
             $scope.selectedProduct = product;
             
